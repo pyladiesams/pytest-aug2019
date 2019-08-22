721dffbee623a1ea5e6cef06827bd01f23e6692e
Anna Nikulina <anna.nikulina@dazn.com>
2019-08-22 16:15:41 +0200

    Restricted accesss to methods

--- circle_test/circle.py
+++ circle_test/circle.py
@@ -1,24 +1,24 @@
 class Circle():
     def __init__(self, radius):
-        self._radius = radius
-        self._surface_area = None
+        self.__radius = radius
+        self.__surface_area = None
 
     @property
     def radius(self):
-        return self._radius
+        return self.__radius
 
     @radius.setter
     def radius(self, value):
-        self._radius = value
+        self.__radius = value
 
     @property
     def surface_area(self):
-        self._surface_area = self.surface_area_circle()
-        return self._surface_area
+        self.__surface_area = self.surface_area_circle()
+        return self.__surface_area
 
     @surface_area.setter
     def surface_area(self, value):
-        self._surface_area = value
+        self.__surface_area = value
 
     def surface_area_circle(self):
         return 3.14 * self.radius * self.radius
